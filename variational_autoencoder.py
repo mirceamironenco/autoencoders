95801cd482c1251ee7afbe81aecc13bf234e70af
MironencoMircea <mircea.mironenco@gmail.com>
2017-02-05 17:59:04 +0100

    added autoencoder files to implement

---
+++ variational_autoencoder.py
@@ -0,0 +1,9 @@
+import argparse
+import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
+import numpy as np
+import matplotlib.pyplot as plt
+
+BATCH_SIZE = 50
+LEARNING_RATE = 1e-3
+EPOCHS = 5
