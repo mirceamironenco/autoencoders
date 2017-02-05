7bc655e7cc154714f18a1ab50a605e432056caec
MironencoMircea <mircea.mironenco@gmail.com>
2017-02-05 22:30:44 +0100

    Initial conv impl

--- convolutional_autoencoder.py
+++ convolutional_autoencoder.py
@@ -9,19 +9,19 @@ LEARNING_RATE = 1e-3
 EPOCHS = 5
 
 class ConvolutionalAutoencoder(object):
-	def __init__(self):
-		raise NotImplemented
+	def __init__(self, learning_rate=1e-3):
+		self.learning_rate = learning_rate
 
 	def inference(self, x, color_channels=1,
-	              num_filters=(16, 16),
-	              filter_sizes=(3, 3)):
+				  output_sizes=(16, 16),
+				  filter_sizes=(3, 3)):
 
-		if len(num_filters) != len(filter_sizes):
+		if len(output_sizes) != len(filter_sizes):
 			raise ValueError('Different number of filters specified for inference.')
 
 		if len(x.get_shape()) not in [2, 4]:
 			raise ValueError('Wrong input dimension. Should be (batch_size, vector_repr) '
-			                 'or (batch_size, width, height, color_channels)')
+							 'or (batch_size, width, height, color_channels)')
 
 		elif len(x.get_shape()) == 2:
 			# Reshape to (batch_size, width, height, color_channels)
@@ -31,15 +31,63 @@ class ConvolutionalAutoencoder(object):
 			# Dimension is (batch, w, h, d)
 			layer_activation = x
 
+		# Construct encoder
+		input_depth = color_channels
 
+		# Store weights to be reused
+		encoder = []
 
-		raise NotImplemented
+		# Store output shapes to be used for
+		# deconvolution operation, output_shape parameter.
+		output_shapes = []
+		for filter_index, filter_size in enumerate(filter_sizes):
+			# Current output activation size (# of filters).
+			output_size = output_sizes[filter_index]
+
+			# Initialize weights
+			W = tf.Variable(tf.random_uniform([filter_size, filter_size,
+											   input_depth, output_size],
+											  minval=-1.0 / np.sqrt(input_depth),
+											  maxval=1.0 / np.sqrt(input_depth)))
+			b = tf.Variable(tf.zeros([output_size]))
+
+			# Save weights to be used by decoder (tied weights)
+			encoder.append(W)
+			output_shapes.append(layer_activation.get_shape().as_list())
+
+			# Get layer output activation
+			convolution = tf.bias_add(tf.nn.conv2d(layer_activation, filter=W,
+											  strides=[1, 2, 2, 1], padding='SAME'), b)
+			layer_activation = tf.nn.elu(convolution)
+
+			input_depth = layer_activation.get_shape().as_list()[3]
+
+		encoder.reverse()
+		output_shapes.reverse()
+
+		# Construct decoder
+		# Note: tf.nn.conv2d_transpose is sometimes called 'Deconvolution', but it is
+		# transpose(gradient) of conv2d.
+		for weight_index, weight in enumerate(encoder):
+			W = weight
+			b = tf.Variable(tf.zeros([W.get_shape().as_list()[2]]))
+			s = output_shapes[weight_index]
+			deconvolution = tf.bias_add(tf.nn.conv2d_transpose(layer_activation, W,
+			                                                   tf.pack([tf.shape(x)[0], s[1],
+			                                                           s[2], s[3]]),
+			                                                   strides=[1, 2, 2, 1],
+			                                                   padding='SAME'), b)
+			layer_activation = tf.nn.elu(deconvolution)
+
+		output = layer_activation
+		raise output
 
 	def loss(self, outputs, labels):
-		raise NotImplemented
+		return tf.reduce_sum(tf.square(outputs - labels))
 
 	def optimize(self, loss):
-		raise NotImplemented
+		optimizer = tf.train.AdamOptimizer(learning_rate=self.learning_rate)
+		return optimizer.minimize(loss)
 
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser()
