616ef7b0c0512702edf336971b99e6e294a14064
MironencoMircea <mircea.mironenco@gmail.com>
2017-02-05 22:46:00 +0100

    pipeline defined

--- convolutional_autoencoder.py
+++ convolutional_autoencoder.py
@@ -3,29 +3,31 @@ import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
 import numpy as np
 import matplotlib.pyplot as plt
+from helpers.utils import print_flags
 
 BATCH_SIZE = 50
 LEARNING_RATE = 1e-3
 EPOCHS = 5
+
 
 class ConvolutionalAutoencoder(object):
 	def __init__(self, learning_rate=1e-3):
 		self.learning_rate = learning_rate
 
 	def inference(self, x, color_channels=1,
-				  output_sizes=(16, 16),
-				  filter_sizes=(3, 3)):
+	              output_sizes=(12, 12),
+	              filter_sizes=(3, 3)):
 
 		if len(output_sizes) != len(filter_sizes):
 			raise ValueError('Different number of filters specified for inference.')
 
 		if len(x.get_shape()) not in [2, 4]:
 			raise ValueError('Wrong input dimension. Should be (batch_size, vector_repr) '
-							 'or (batch_size, width, height, color_channels)')
+			                 'or (batch_size, width, height, color_channels)')
 
 		elif len(x.get_shape()) == 2:
 			# Reshape to (batch_size, width, height, color_channels)
-			square_dim = np.sqrt(x.get_shape().as_list()[1])
+			square_dim = int(np.sqrt(x.get_shape().as_list()[1]))
 			layer_activation = tf.reshape(x, [-1, square_dim, square_dim, color_channels])
 		else:
 			# Dimension is (batch, w, h, d)
@@ -46,9 +48,9 @@ class ConvolutionalAutoencoder(object):
 
 			# Initialize weights
 			W = tf.Variable(tf.random_uniform([filter_size, filter_size,
-											   input_depth, output_size],
-											  minval=-1.0 / np.sqrt(input_depth),
-											  maxval=1.0 / np.sqrt(input_depth)))
+			                                   input_depth, output_size],
+			                                  minval=-1.0 / np.sqrt(input_depth),
+			                                  maxval=1.0 / np.sqrt(input_depth)))
 			b = tf.Variable(tf.zeros([output_size]))
 
 			# Save weights to be used by decoder (tied weights)
@@ -56,8 +58,9 @@ class ConvolutionalAutoencoder(object):
 			output_shapes.append(layer_activation.get_shape().as_list())
 
 			# Get layer output activation
-			convolution = tf.bias_add(tf.nn.conv2d(layer_activation, filter=W,
-											  strides=[1, 2, 2, 1], padding='SAME'), b)
+			convolution = tf.nn.bias_add(tf.nn.conv2d(layer_activation, filter=W,
+			                                       strides=[1, 2, 2, 1], padding='SAME'),
+			                          b)
 			layer_activation = tf.nn.elu(convolution)
 
 			input_depth = layer_activation.get_shape().as_list()[3]
@@ -72,15 +75,16 @@ class ConvolutionalAutoencoder(object):
 			W = weight
 			b = tf.Variable(tf.zeros([W.get_shape().as_list()[2]]))
 			s = output_shapes[weight_index]
-			deconvolution = tf.bias_add(tf.nn.conv2d_transpose(layer_activation, W,
-			                                                   tf.pack([tf.shape(x)[0], s[1],
-			                                                           s[2], s[3]]),
+			deconvolution = tf.nn.bias_add(tf.nn.conv2d_transpose(layer_activation, W,
+			                                                   tf.pack(
+				                                                   [tf.shape(x)[0], s[1],
+				                                                    s[2], s[3]]),
 			                                                   strides=[1, 2, 2, 1],
 			                                                   padding='SAME'), b)
 			layer_activation = tf.nn.elu(deconvolution)
 
 		output = layer_activation
-		raise output
+		return output
 
 	def loss(self, outputs, labels):
 		return tf.reduce_sum(tf.square(outputs - labels))
@@ -89,6 +93,32 @@ class ConvolutionalAutoencoder(object):
 		optimizer = tf.train.AdamOptimizer(learning_rate=self.learning_rate)
 		return optimizer.minimize(loss)
 
+def main(_):
+	print_flags(FLAGS)
+
+	input_placeholder = tf.placeholder(tf.float32, shape=[None, 784])
+	model = ConvolutionalAutoencoder(learning_rate=FLAGS.learning_rate)
+	output = model.inference(x=input_placeholder)
+	loss = model.loss(output, input_placeholder)
+	optimization_op = model.optimize(loss)
+
+
+	# Load MNIST data
+	mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
+	X_train = mnist.train.images
+	mean_image = np.mean(X_train, axis=0)
+
+	sess = tf.Session()
+	sess.run(tf.global_variables_initializer())
+
+
+FLAGS = None
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser()
 
+	parser.add_argument('--learning_rate', type=float, default=LEARNING_RATE)
+	parser.add_argument('--batch_size', type=int, default=BATCH_SIZE)
+	parser.add_argument('--epochs', type=int, default=EPOCHS)
+	FLAGS, _ = parser.parse_known_args()
+
+	tf.app.run()
